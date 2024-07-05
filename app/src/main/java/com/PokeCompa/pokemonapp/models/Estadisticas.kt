cd4ad55b8d2c832299653b3a97a3c2bf44ef7794
Saul <saulcordovavill@gmail.com>
2024-07-04 22:46:57 -0400

    add file Pokemon.kt, Estadisticas.kt

---
+++ app/src/main/java/com/PokeCompa/pokemonapp/models/Estadisticas.kt
@@ -0,0 +1,7 @@
+package com.PokeCompa.pokemonapp.models
+
+data class Estadisticas(
+    val hp: Int,
+    val 
+
+)
---
+++ app/src/main/java/com/PokeCompa/pokemonapp/models/Pokemon.kt
@@ -0,0 +1,9 @@
+package com.PokeCompa.pokemonapp.models
+
+data class Pokemon(
+    val name: String,
+    val height: Int,
+    val id: Int,
+    val weight: Int,
+    val base_experience: Int
+)
